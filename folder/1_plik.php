00f917343def7a92db5866ef1ba93ef156d1e280
Rogas17 <dorogozi01@gmail.com>
2023-03-01 10:57:29 +0100

    modyfikacja pliku

--- folder/1_plik.php
+++ folder/1_plik.php
@@ -1,3 +1,13 @@
 <?php
-    echo "wsb";
+    $firstname = "Dominik";
+    $lastname = "Rogoziński";
+    echo "imie i nazwisko: $firstname $lastname<br>";
+    echo 'imie i nazwisko: $firstname $lastname<br>';
+
+    //heredoc
+    echo <<< DATA
+        <hr>
+        Imię: $firstname<br>
+        Nazwisko: $lastname<br>
+DATA;
 ?>
